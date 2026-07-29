f185a1f9de8109f5fdc0ded5ee67c6a283809656
ceribas-wx16 <ceribasbso6716@gmail.com>
2026-07-29 12:26:58 +0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,14 @@
+const SUPABASE_URL = "BURAYA_SUPABASE_URL";
+
+const SUPABASE_ANON_KEY = "BURAYA_ANON_KEY";
+
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
+
+
+// Global kullanım için
+
+window.supabase = supabaseClient;
