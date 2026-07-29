8e28dac249b3fa1858299a718af3567a79cbd086
ceribas-wx16 <ceribasbso6716@gmail.com>
2026-07-29 12:57:20 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -3,10 +3,10 @@ const SUPABASE_URL = "https://nnvbxtjpfpfwegewtnkc.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5udmJ4dGpwZnBmd2VnZXd0bmtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyNDE3NTcsImV4cCI6MjEwMDgxNzc1N30.-2FbxS3SAkvvKUZbBC2kPNR_mMRk4P7vG-kz6eES3G4";
 
 
-const supabaseClient = window.supabase.createClient(
+const supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
 
 
-window.supabaseClient = supabaseClient;
+window.supabase = supabaseClient;
