dbc0c8482fef24b79afd10a150324ee3468c8051
ceribas-wx16 <ceribasbso6716@gmail.com>
2026-08-03 17:42:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+document.addEventListener("DOMContentLoaded", async () => {
 
+    await anaSayfaUrunleriniGetir();
+
+});
+
+async function anaSayfaUrunleriniGetir(){
+
+    const { data, error } = await supabase
+        .from("products")
+        .select("*")
+        .eq("is_active", true)
+        .order("created_at", { ascending:false });
+
+    if(error){
+
+        console.log(error);
+
+        return;
+
+    }
+
+    console.log("Ürünler:", data);
+
+}
